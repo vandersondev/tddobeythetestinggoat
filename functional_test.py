97cf28f6c24d778b6746d1a8b78653c70a9deac7
Vanderson <vandersonwebdev@gmail.com>
2020-05-22 01:29:10 -0300

    First FT specced out in comments, and now uses unittest.

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,21 @@
+import unittest
+
 from selenium import webdriver
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
