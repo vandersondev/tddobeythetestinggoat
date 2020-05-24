0f591478bf4e22540e68ed61ef310be70fd4f74b
Vanderson <vandersonwebdev@gmail.com>
2020-05-23 22:48:39 -0300

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -15,7 +15,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-do', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         inputbox = self.browser.find_element_by_id('id_new_item')
