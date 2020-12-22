262a02132b09bd1ad8998b430f1159fc6bba9a9b
Vanderson <vandersonwebdev@gmail.com>
2020-12-22 03:05:17 -0300

    correct flak8 warning

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 auth.signals.user_logged_in.disconnect(auth.models.update_last_login)
 
+
 class User(models.Model):
 
     email = models.EmailField(primary_key=True)
