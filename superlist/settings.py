120c0d84faa6be3494fc6234d2889e2f51c9ea93
Vanderson <vandersonwebdev@gmail.com>
2020-12-27 04:13:10 -0300

    comment django heroku config

--- superlist/settings.py
+++ superlist/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,4 +137,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Heroku Settings
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
