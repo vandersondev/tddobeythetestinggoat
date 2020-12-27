e3a4d2aa4f6617ed0199a24d17c3696aeb8b8222
Vanderson <vandersonwebdev@gmail.com>
2020-12-27 04:36:00 -0300

    remove protocol from stagingserver test env variable

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -28,7 +28,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
-            self.live_server_url = 'https://' + staging_server
+            self.live_server_url = staging_server
 
     def tearDown(self):
         self.browser.quit()
